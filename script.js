6e6a012f74fa62855fa6abc957ecd2db2a275652
Lawrence-oss <lawrence.ejiofor@Ethnoscyber.com>
2026-02-07 15:26:53 +0100

    added refractors to make code cleaner

--- script.js
+++ script.js
@@ -5,81 +5,39 @@ let resetBtn = document.getElementById("reset-btn");
 let minusBtn = document.getElementById("minus-btn");
 let message = document.getElementById("message");
 
-clickBtn.addEventListener("click", () => {
-  count = count + 1;
-  countDisplay.textContent = count;
-
- if (count >= 60) {
+function updateColorAndMessage() {
+  if (count >= 60) {
     countDisplay.style.color = "green";
     message.textContent = "Congratulations! You've reached 60 clicks!";
-  }
-  else if (count >= 20) {
+  } else if (count >= 20) {
     countDisplay.style.color = "red";
     message.textContent = "You've reached 20 clicks!";
-  }
-  else if (count >=10) {
+  } else if (count >= 10) {
     countDisplay.style.color = "orange";
     message.textContent = "You've reached 10 clicks!";
   } else {
     countDisplay.style.color = "black";
     message.textContent = "";
   }
+}
+
+clickBtn.addEventListener("click", () => {
+  count++;
+  countDisplay.textContent = count;
+  updateColorAndMessage();
 });
+
+minusBtn.addEventListener("click", () => {
+  if (count > 0) {
+    count--;
+    countDisplay.textContent = count;
+  }
+  updateColorAndMessage();
+});
+
 resetBtn.addEventListener("click", () => {
   count = 0;
   countDisplay.textContent = count;
   countDisplay.style.color = "black";
+  message.textContent = "";
 });
-
-
-  minusBtn.addEventListener("click", () => {
-    if (count > 0) {
-      count = count - 1;
-      countDisplay.textContent = count;
-    }
-      if (count >= 60) {
-    countDisplay.style.color = "green";
-    message.textContent = "Congratulations! You've reached 60 clicks!";
-  }
-  else if (count >= 20) {
-    countDisplay.style.color = "red";
-    message.textContent = "You've reached 20 clicks!";
-  }
-  else if (count >=10) {
-    countDisplay.style.color = "orange";
-    message.textContent = "You've reached 10 clicks!";
-  } else {
-    countDisplay.style.color = "black";
-    message.textContent = "";
-  }
-
-  });
-
-  //   if (count <= 60) {
-  //     countDisplay.style.color = "red";
-  //   }
-    
-  //   if (count <= 20) {
-  //     countDisplay.style.color = "orange";
-  //   }
-
-  //   if (count <= 10) {
-  //     countDisplay.style.color = "black";
-  //   }
-  // });
-
-   if (count >= 60) {
-    countDisplay.style.color = "green";
-    message.textContent = "Congratulations! You've reached 60 clicks!";
-  }
-  else if (count >= 20) {
-    countDisplay.style.color = "red";
-    message.textContent = "You've reached 20 clicks!";
-  }
-  else if (count >=10) {
-    countDisplay.style.color = "orange";
-    message.textContent = "You've reached 10 clicks!";
-  } else {
-    countDisplay.style.color = "black";
-    message.textContent = "";
-  }
